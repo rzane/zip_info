990ef7e67929dabd0e46f6d1126ec3586ae06585
Ray Zane <raymondzane@gmail.com>
2020-10-31 22:31:51 -0400

    Parsing some more information

--- lib/zip_info.ex
+++ lib/zip_info.ex
@@ -5,61 +5,70 @@ defmodule ZipInfo do
   See: https://en.wikipedia.org/wiki/Zip_(file_format)#Local_file_header
   """
 
-  defstruct [:name, :compressed_size, :uncompressed_size]
+  defstruct [
+    :filename,
+    :filename_length,
+    :extra,
+    :extra_length,
+    :compressed_size,
+    :uncompressed_size
+  ]
 
   @type error :: File.posix() | :badarg | :terminated
 
   @type t :: %__MODULE__{
-          name: binary(),
+          filename: binary(),
+          filename_length: non_neg_integer(),
+          extra: binary(),
+          extra_length: non_neg_integer(),
           compressed_size: non_neg_integer(),
           uncompressed_size: non_neg_integer()
         }
-
-  defmacrop uint(size) do
-    quote do: little - signed - integer - size(unquote(size))
-  end
 
   @doc false
   @spec read(IO.device()) :: {:ok, t()} | {:error, error} | :eof
   def read(io) do
     with {:ok, data} <- binread(io, 30),
-         {:ok, header, meta} <- parse(data),
-         {:ok, name} <- binread(io, meta.name_size),
-         {:ok, _} <- shift(io, meta.extra_size),
-         {:ok, _} <- shift(io, header.compressed_size) do
-      {:ok, %__MODULE__{header | name: name}}
+         {:ok, header} <- parse(data),
+         {:ok, filename} <- binread(io, header.filename_length),
+         {:ok, extra} <- binread(io, header.extra_length),
+         {:ok, _} <- ignore(io, header.compressed_size) do
+      {:ok, %__MODULE__{header | filename: filename, extra: extra}}
     end
   end
 
   defp binread(io, count) do
     case IO.binread(io, count) do
-      :eof -> {:error, :eof}
+      :eof -> :eof
       {:error, reason} -> {:error, reason}
       value -> {:ok, value}
     end
   end
 
-  defp shift(io, offset) do
+  defp ignore(io, offset) do
     :file.position(io, {:cur, offset})
+  end
+
+  defmacrop uint(size) do
+    quote do: little - signed - integer - size(unquote(size))
   end
 
   defp parse(
          <<80, 75, 3, 4, _version::uint(16), _flag::uint(16), _compression_method::uint(16),
            _mtime::uint(16), _mdate::uint(16), _crc32::uint(32), compressed_size::uint(32),
-           uncompressed_size::uint(32), name_size::uint(16), extra_size::uint(16)>>
+           uncompressed_size::uint(32), filename_length::uint(16), extra_length::uint(16)>>
        ) do
     header = %__MODULE__{
       compressed_size: compressed_size,
-      uncompressed_size: uncompressed_size
+      uncompressed_size: uncompressed_size,
+      filename_length: filename_length,
+      extra_length: extra_length
     }
 
-    meta = %{
-      name_size: name_size,
-      extra_size: extra_size
-    }
-
-    {:ok, header, meta}
+    {:ok, header}
   end
 
-  defp parse(_), do: :eof
+  defp parse(_) do
+    {:error, :corrput}
+  end
 end
--- test/zip_info_test.exs
+++ test/zip_info_test.exs
@@ -5,10 +5,10 @@ defmodule ZipInfoTest do
   @tag io: "test/fixtures/fixture.zip"
   test "ZipInfo.read/1", %{io: io} do
     assert {:ok, a} = ZipInfo.read(io)
-    assert a.name == "a.txt"
+    assert a.filename == "a.txt"
 
     assert {:ok, b} = ZipInfo.read(io)
-    assert b.name == "b.txt"
+    assert b.filename == "b.txt"
 
     assert :eof = ZipInfo.read(io)
   end
