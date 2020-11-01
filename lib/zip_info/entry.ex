840c49b3d3f73b73f687e401a44a842bc27c3786
Ray Zane <raymondzane@gmail.com>
2020-11-01 03:30:40 -0500

    Parse an entry

--- lib/zip_info/entry.ex
+++ lib/zip_info/entry.ex
@@ -1,12 +1,48 @@
 defmodule ZipInfo.Entry do
-  defstruct [:name, :size, :compressed_size, :position]
+  @size 46
+  @signature <<0x50, 0x4B, 0x01, 0x02>>
 
-  @type position :: {non_neg_integer(), non_neg_integer()}
+  defstruct [
+    :name,
+    :comment,
+    :compressed_size,
+    :size,
+    :offset
+  ]
 
-  @type t :: %__MODULE__{
-          name: binary(),
-          size: non_neg_integer(),
-          compressed_size: non_neg_integer(),
-          position: position()
-        }
+  def read(io, offset \\ :cur) do
+    with {:ok, data} <- :file.pread(io, offset, @size),
+         {:ok, entry, sizes} <- parse(data),
+         {:ok, name} <- :file.read(io, sizes.name),
+         {:ok, _} <- :file.position(io, {:cur, sizes.extra}),
+         {:ok, comment} <- :file.read(io, sizes.comment) do
+      {:ok, %__MODULE__{entry | name: name, comment: comment}}
+    end
+  end
+
+  defp parse(<<
+         @signature,
+         _version::little-size(16),
+         _min_version::little-size(16),
+         _flags::little-size(16),
+         _compression_method::little-size(16),
+         _mtime::little-size(16),
+         _mdate::little-size(16),
+         _crc32::binary-size(4),
+         compressed_size::little-size(32),
+         size::little-size(32),
+         name_length::little-size(16),
+         extra_length::little-size(16),
+         comment_length::little-size(16),
+         _disk_start::little-size(16),
+         _internal_file_attributes::little-size(16),
+         _external_file_attributes::little-size(32),
+         offset::little-size(32)
+       >>) do
+    entry = %__MODULE__{size: size, compressed_size: compressed_size, offset: offset}
+    sizes = %{name: name_length, extra: extra_length, comment: comment_length}
+    {:ok, entry, sizes}
+  end
+
+  defp parse(_), do: {:error, :invalid}
 end
--- test/zip_info_test.exs
+++ test/zip_info_test.exs
@@ -2,11 +2,31 @@ defmodule ZipInfoTest do
   use ZipInfo.Case
 
   alias ZipInfo.CentralDirectory
+  alias ZipInfo.Entry
 
   @tag io: "test/fixtures/fixture.zip"
   test "CentralDirectory.find/1", %{io: io} do
     assert {:ok, directory} = CentralDirectory.find(io)
     assert directory.count == 2
     assert directory.start == 170
+  end
+
+  @tag io: "test/fixtures/fixture.zip"
+  test "Entry.read/2", %{io: io} do
+    assert {:error, :invalid} = Entry.read(io)
+
+    assert {:ok, entry} = Entry.read(io, 170)
+    assert entry.name == "a.txt"
+    assert entry.size == 22
+    assert entry.compressed_size == 22
+    assert entry.offset == 0
+    assert entry.comment == ""
+
+    assert {:ok, entry} = Entry.read(io)
+    assert entry.name == "b.txt"
+    assert entry.size == 22
+    assert entry.compressed_size == 22
+    assert entry.offset == 85
+    assert entry.comment == ""
   end
 end
